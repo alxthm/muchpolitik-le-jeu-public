5d4a6ca0cd2f136841a2fa68d5749e9b6d74b1bb
garbadrom <33730312+garbadrom@users.noreply.github.com>
2017-11-19 18:05:02 +0100

    added tutorial level and transparency to cutscene characters

---
+++ core/src/com/muchpolitik/lejeu/GameActors/GameObjects/Hint.java
@@ -0,0 +1,10 @@
+package com.muchpolitik.lejeu.GameActors.GameObjects;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by alexandre on 19/11/2017.
+ */
+
+public class Hint extends Actor {
+}
